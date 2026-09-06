8943782f86f827f49fe91ead5f756136f650cf57
Your Name <qianxuntudou@gmail.com>
2026-09-06 19:01:37 +0800

    v1.6.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         applicationId = (project.findProperty("applicationId") as? String) ?: "com.tutu.myblbl"
         minSdk = 23
         targetSdk = 35
-        versionCode = 77
-        versionName = "1.6.17"
+        versionCode = 78
+        versionName = "1.6.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
